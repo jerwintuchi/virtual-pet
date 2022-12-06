2dbe3c70673a4536a9a0d634ae63d2f1acbaf7c3
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2022-12-06 19:06:41 +0800

    Update wiggie.py
    added comment

--- wiggie.py
+++ wiggie.py
@@ -11,7 +11,7 @@ sleep_num = [10,11,12,13,15]
 walk_left = [6,7]
 walk_right = [8,9]
 event_number = random.randrange(1,3,1)
-impath = 'C:\\Virtual Pets\\Wiggie the Green Alien\\images\\'
+impath = 'C:\\Virtual Pets\\Wiggie the Green Alien\\images\\' #name of directory where the .gifs files are located
 
 #detect path for .exe file
 application_path = os.path.dirname(sys.executable)
